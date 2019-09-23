f5cddf5aefad5effec96daf21e944a3221b02898
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-09-23 16:40:01 +0100

    updated wording

--- views/components/phase-banner.jsx
+++ views/components/phase-banner.jsx
@@ -6,12 +6,12 @@ class PhaseBanner extends React.Component {
     return (
       <div className="govuk-phase-banner">
         <div className="govuk-width-container">
-          <h1>This service will be retired on 31st October 2019.</h1>
+          <h1>This service will no longer be available from 31st October 2019.</h1>
           <p>
             If you have applications you wish to continue working on, please download them using the &quot;Backup(.ppl)&quot; link.
           </p>
           <p>
-            Upload the .ppl files to the new ASPeL at <a href="http://external.aspel.homeoffice.gov.uk">external.aspel.homeoffice.gov.uk</a> where
+            You can then upload your .ppl file to <a href="http://external.aspel.homeoffice.gov.uk">external.aspel.homeoffice.gov.uk</a> where
             you can resume drafting your application.
           </p>
         </div>
