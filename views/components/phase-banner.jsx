cd4ae783caf6c5ee8083e1c6cdafe73f0d7cd680
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-09-23 15:12:41 +0100

    indent

--- views/components/phase-banner.jsx
+++ views/components/phase-banner.jsx
@@ -3,18 +3,20 @@ import React from 'react';
 class PhaseBanner extends React.Component {
 
   render() {
-    return <div className="govuk-phase-banner">
-      <div className="govuk-width-container">
-        <h1>This service will be retired on 31st October 2019.</h1>
-        <p>
-          If you have applications you wish to continue working on, please download them using the &quot;Backup(.ppl)&quot; link.
-        </p>
-        <p>
-          Upload the .ppl files to the new ASPeL at <a href="http://external.aspel.homeoffice.gov.uk">external.aspel.homeoffice.gov.uk</a> where
-          you can resume drafting your application.
-        </p>
+    return (
+      <div className="govuk-phase-banner">
+        <div className="govuk-width-container">
+          <h1>This service will be retired on 31st October 2019.</h1>
+          <p>
+            If you have applications you wish to continue working on, please download them using the &quot;Backup(.ppl)&quot; link.
+          </p>
+          <p>
+            Upload the .ppl files to the new ASPeL at <a href="http://external.aspel.homeoffice.gov.uk">external.aspel.homeoffice.gov.uk</a> where
+            you can resume drafting your application.
+          </p>
+        </div>
       </div>
-    </div>;
+    );
   }
 
 }
