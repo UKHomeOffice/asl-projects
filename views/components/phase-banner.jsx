84df39be055a3c4055a93c9883cc8dc71c9f067d
Joe Fitter <joefitter@hotmail.com>
2020-02-05 12:20:11 +0000

    retire service

--- views/components/phase-banner.jsx
+++ views/components/phase-banner.jsx
@@ -6,13 +6,9 @@ class PhaseBanner extends React.Component {
     return (
       <div className="govuk-phase-banner">
         <div className="govuk-width-container">
-          <h1>This service will no longer be available from 31st January 2020.</h1>
+          <h1>This service is no longer available.</h1>
           <p>
-            If you have applications you wish to continue working on, please download them using the &quot;Backup(.ppl)&quot; link.
-          </p>
-          <p>
-            You can then upload your .ppl file to <a href="http://external.aspel.homeoffice.gov.uk">external.aspel.homeoffice.gov.uk</a> where
-            you can resume drafting your application.
+            To apply for a project licence, speak to the Home Office liaison contact at your establishment.
           </p>
         </div>
       </div>
--- views/index.jsx
+++ views/index.jsx
@@ -4,13 +4,7 @@ import Layout from './layout';
 class Index extends React.Component {
 
   render() {
-    return <Layout {...this.props}>
-      <main className="main govuk-main-wrapper" id="content">
-        <div className="govuk-width-container">
-          <div id="ppl-drafting-tool"/>
-        </div>
-      </main>
-    </Layout>
+    return <Layout {...this.props} />
   }
 
 }
