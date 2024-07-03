834386fe76ea946b7e85f4ea560312e8f678a673
farrukhishaq <farrukh.ishaq@homeoffice.gov.uk>
2024-07-03 09:31:54 +0100

    aspel to use redux dev tools.

--- client/store.js
+++ client/store.js
@@ -1,13 +1,17 @@
-import {createStore, applyMiddleware} from 'redux';
-import reducer from './reducers';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import reducer from './reducers';
 
 const middlewares = [thunk];
 
 export default function configureStore(initialState = {}) {
-  return createStore(
-    reducer,
-    initialState,
-    applyMiddleware(...middlewares)
+  // Use composeWithDevTools to enhance the store with middleware and ReduxDevTools
+  const store = createStore(
+      reducer,
+      initialState,
+      composeWithDevTools(applyMiddleware(...middlewares))
   );
+
+  return store;
 }
