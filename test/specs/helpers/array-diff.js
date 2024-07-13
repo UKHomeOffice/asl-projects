b2d28594996c0fda9634ae861a1bf2461faad174
kiran-varma-home-office <kiran.varma@digital.homeoffice.gov.uk>
2024-07-13 12:30:01 +0100

    test: added unit tests

--- test/specs/helpers/array-diff.js
+++ test/specs/helpers/array-diff.js
@@ -1,13 +1,22 @@
+import assert from 'assert';
 import { findArrayDifferences } from '../../../client/helpers/array-diff';
 
-const first = ['kiran', 'varma', 'new1', 'remove-some-chars'];
+const first = ['cancer', 'stem cells', 'therapy', 'other research'];
 
-const second = ['kiran', 'var', '', 'remove-chars'];
+const second = ['cancer', 'stem', 'therapy', 'new research', 'second new research'];
 
-describe.only('array-diff', () => {
-  it('should use diff', () => {
-    const diff = findArrayDifferences(first, second);
-    console.log('diff--->', JSON.stringify(diff));
+describe('array-diff', () => {
+  it('should find array items that are same and new items', () => {
+    const added = findArrayDifferences(first, second).added;
 
+    const expectedResult = [{'count': 2, 'value': ['cancer', 'therapy']}, {'count': 3, 'added': true, 'value': ['stem', 'new research', 'second new research']}];
+    assert.deepEqual(added, expectedResult);
+  });
+
+  it('should find array items that are same and items that are removed', () => {
+    const removed = findArrayDifferences(first, second).removed;
+
+    const expectedResult = [{'count': 2, 'value': ['cancer', 'therapy']}, {'count': 2, 'removed': true, 'value': ['stem cells', 'other research']}];
+    assert.deepEqual(removed, expectedResult);
   });
 });
