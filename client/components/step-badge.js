cf002d4ac7824cc2d80f23f4d5580804b90f7ae4
Parvin Baishkiyar <Parvin.Baishkiyar@digital.homeoffice.gov.uk>
2024-09-25 10:44:52 +0100

    Update client/components/step-badge.js
    Co-authored-by: Jeff Horton <87995501+jeff-horton-ho-sas@users.noreply.github.com>

--- client/components/step-badge.js
+++ client/components/step-badge.js
@@ -8,7 +8,7 @@ const changeFields = (step, prefix) => step.reusable ? [ `reusableSteps.${step.r
 export default function StepBadge(props) {
   const { previous, steps, firstSteps, grantedSteps } = useSelector(state => state.application.previousProtocols);
   if (!previous || !steps || !firstSteps || !grantedSteps) {
-    return <></>;
+    return null;
   }
 
   let stepIds = [];
