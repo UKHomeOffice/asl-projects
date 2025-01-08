2044197ac876f119f5041edc1856643ba82ee0a6
kiran-varma-home-office <kiran.varma@digital.homeoffice.gov.uk>
2025-01-08 15:45:06 +0000

    wip

--- client/components/download-link/components/training-summary-renderer.js
+++ client/components/download-link/components/training-summary-renderer.js
@@ -1,33 +1,29 @@
+import { Table, TableRow, TableCell, Paragraph } from 'docx';
+
 export function trainingSummaryRenderer(doc, values, renderTable) {
   doc.createParagraph('There was a problem rendering this content');
   doc.createParagraph('There was a problem rendering this content new');
   doc.createParagraph(JSON.stringify(values.training, null, 2));
-  // const table = new Table({
-  //   rows: [
-  //     new TableRow({
-  //       children: [
-  //         new TableCell({
-  //           children: [new Paragraph('Hello')]
-  //         }),
-  //         new TableCell({
-  //           children: []
-  //         })
-  //       ]
-  //     }),
-  //     new TableRow({
-  //       children: [
-  //         new TableCell({
-  //           children: []
-  //         }),
-  //         new TableCell({
-  //           children: [new Paragraph('World')]
-  //         })
-  //       ]
-  //     })
-  //   ]
-  // });
+  const table = new Table({
+    rows: [
+      new TableRow([
+        new TableCell({
+          children: [new Paragraph('Hello')]
+        }),
+        new TableCell({
+          children: []
+        })
+      ]),
+      new TableRow([
+        new TableCell({
+          children: []
+        }),
+        new TableCell({
+          children: [new Paragraph('World')]
+        })
+      ])
+    ]
+  });
 
-  // return table;
-
-  return renderTable(doc, values);
+  doc.addTable(table);
 }
