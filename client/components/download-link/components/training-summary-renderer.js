c9ec3b597953de4f5a503e9125ea8fe13ddfd0e9
kiran-varma-home-office <kiran.varma@digital.homeoffice.gov.uk>
2025-01-10 15:07:56 +0000

    formatting and '-' when there is no data

--- client/components/download-link/components/training-summary-renderer.js
+++ client/components/download-link/components/training-summary-renderer.js
@@ -5,6 +5,12 @@ export function trainingSummaryRenderer(doc, values) {
 
   const training = values.training;
   const rowCount = training.length > 0 && values.training.length;
+
+  if (!rowCount) {
+    doc.createParagraph('No training records found');
+    return;
+  }
+
   const table = new Table({
     rows: rowCount + 1, // +1 for header row
     columns: TRAINING_RECORD_HEADERS.length,
@@ -17,21 +23,29 @@ export function trainingSummaryRenderer(doc, values) {
   });
 
   TRAINING_RECORD_HEADERS.forEach((header, index) => {
-    table.getCell(0, index).createParagraph(header);
+    table.getCell(0, index).createParagraph(header).center();
   });
 
   training.forEach((element, index) => {
-    const category = element.isExemption ? 'Exemption' : 'Certificate';
-    const modules = element.modules;
-    const animalTypes = element.species;
-    const details = [`Certificate number: ${element.certificateNumber}`, `Awarded on: ${element.passDate}`, `Awarded by: ${element.accreditingBody}`];
     const tableRow = index + 1;
 
+    const category = element.isExemption ? 'Exemption' : 'Certificate';
     table.getCell(tableRow, 0).createParagraph(category);
-    modules.forEach(module => table.getCell(tableRow, 1).createParagraph(module).bullet());
-    animalTypes.forEach(animalType => table.getCell(tableRow, 2).createParagraph(animalType).bullet());
+
+    createBulletedParagraphOfItems(element.modules, table, tableRow, 1);
+    createBulletedParagraphOfItems(element.species, table, tableRow, 2);
+
+    const details = [`Certificate number: ${element.certificateNumber}`, `Awarded on: ${element.passDate}`, `Awarded by: ${element.accreditingBody}`];
     details.forEach(detail => table.getCell(tableRow, 3).createParagraph(detail));
   });
 
   doc.addTable(table);
+}
+
+function createBulletedParagraphOfItems(items, table, tableRow, tableColumn) {
+  if (items.length > 0) {
+    items.forEach(item => table.getCell(tableRow, tableColumn).createParagraph(item).bullet());
+  } else {
+    table.getCell(tableRow, 2).createParagraph('-').center();
+  }
 }
