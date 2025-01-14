348ba26a254b73bcedb0cc3bb9f3d8d94cc75726
kiran-varma-home-office <kiran.varma@digital.homeoffice.gov.uk>
2025-01-14 09:05:57 +0000

    improve jsdoc

--- client/components/download-link/helpers/initialize-table.mjs
+++ client/components/download-link/helpers/initialize-table.mjs
@@ -1,12 +1,12 @@
+import { Table } from 'docx';
+
 /**
  * Creates a new table with the specified number of rows and columns.
  *
  * @param {number} rows - The number of rows in the table.
  * @param {number} columns - The number of columns in the table.
- * @returns {Table} A new Table instance with the specified rows and columns and default margins.
+ * @returns {import('docx').Table} A new Table instance with the specified rows and columns and default margins.
  */
-import { Table } from 'docx';
-
 export function initializeTable(rows, columns) {
   return new Table({
     rows,
--- client/components/download-link/helpers/populate-table-header.mjs
+++ client/components/download-link/helpers/populate-table-header.mjs
@@ -1,9 +1,9 @@
 /**
  * Populates the header row of a table with the provided headers.
  *
- * @param {Table} table - The table object to populate.
+ * @param {import('docx').Table} table - The table object to populate.
  * @param {Array<string>} headers - An array of header strings to populate the table header.
- * @returns {Table} The updated table object with the populated headers.
+ * @returns {import('docx').Table} The updated table object with the populated headers.
  */
 export function populateTableHeader(table, headers) {
   headers.forEach((header, index) => {
